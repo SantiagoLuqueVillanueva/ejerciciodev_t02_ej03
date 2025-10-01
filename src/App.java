79f3baed8581c39c245a5bc06071bbadba47f91e
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-01 20:00:29 +0200

    Ejercicio hecho

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String name = "Santiago Luque Villanueva";
+        String addres = "Calle java, 33 - code (Málaga) - España";
+        String num = "111-222-333";
+
+        System.out.println(name);
+        System.out.println(addres);
+        System.out.println(num);
     }
 }
